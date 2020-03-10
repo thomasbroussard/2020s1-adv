781747d11d1092669bd5e3488942ed14806bd47c
Thomas <thomas.broussard@gmail.com>
2020-03-10 20:11:33 +0100

    * dao integration

---
+++ quiz-core/src/main/java/fr/epita/quiz/services/QuestionDAO.java
@@ -0,0 +1,28 @@
+package fr.epita.quiz.services;
+
+import java.util.List;
+
+import fr.epita.quiz.datamodel.Question;
+
+public class QuestionDAO {
+	
+	public void create(Question question) {
+		
+	}
+	public void update(Question question) {
+		
+	}
+	public void delete(Question question) {
+		
+	}
+	
+	public List<Question> search(Question question){
+		return null;
+	}
+	public Question getById(Long id){
+		return null;
+	}
+	
+	
+
+}
