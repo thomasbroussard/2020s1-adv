ec5b1b82f11f720e60471531eb5e8fc5b1baf899
User <User@DESKTOP-211D058>
2020-03-30 19:51:55 +0200

    * jersey integration

--- quiz-rest/src/main/java/fr/epita/quiz/resources/ExamResource.java
+++ quiz-rest/src/main/java/fr/epita/quiz/resources/ExamResource.java
@@ -1,10 +1,16 @@
 package fr.epita.quiz.resources;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import fr.epita.quiz.datamodel.Answer;
 import fr.epita.quiz.services.business.ExamDataService;
 
 @Path("/exam")
@@ -15,14 +21,23 @@ public class ExamResource {
 	
 	@POST
 	@Path("/answer")
+
 	public void addAnswerToQuestion() {
 		System.out.println("testPost");
+	
 	}
 	
 	@GET
 	@Path("/answer")
-	public void getAnswers() {
-		System.out.println("testGet");
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response getAnswers() {
+		
+		//beginning dummy implementation
+		Answer answer = new Answer();
+		answer.setContent("This is a sampleAnswer");
+		
+		
+		return Response.ok(Arrays.asList(answer)).build();
 	}
 	
 	
